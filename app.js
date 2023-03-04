a98d102568d5c08968d841773eb722f63134e390
Anatolie Sernii <asernii@sleed.gr>
2023-03-04 02:26:45 +0200

    Live update3

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World Live2');
+  res.end('Hello World Live3');
 });
 
 server.listen(port, hostname, () => {
